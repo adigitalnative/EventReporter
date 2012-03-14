9a2fcfc52f6668a7bdc9f6eb7f1d21271b55fc12
ADigitalNative <adigitalnative@gmail.com>
2012-03-14 17:28:37 -0400

    Load now load attendees into DataParser.get_attendees

--- data_parser.rb
+++ data_parser.rb
@@ -1,15 +1,15 @@
 require 'csv'
-require './eventmanager'
 
 module EventReporter
   class DataParser
     def self.load(filename)
       "Loading the data from #{filename[0]}"
-      @data = CSV.open(filename[0], :headers => true, :header_converters => :symbol)
+      @attendees = CSV.open(filename[0], :headers => true, :header_converters => :symbol)
+      get_attendees
+    end
 
-      # @data.each do |line|
-      #   puts [line[:first_name]]
-      # end
+    def self.get_attendees
+      @attendees
     end
 
     def self.valid_parameters?(parameters)
