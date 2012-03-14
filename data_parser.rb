97d976204bbd895d9a26c47d1f48f9242e5aeb85
ADigitalNative <adigitalnative@gmail.com>
2012-03-14 14:17:54 -0400

    DataParser loads CSV into @data

--- data_parser.rb
+++ data_parser.rb
@@ -1,7 +1,15 @@
+require 'csv'
+require './eventmanager'
+
 module EventReporter
   class DataParser
     def self.load(filename)
-      "Loading the data from #{filename}!"
+      "Loading the data from #{filename[0]}"
+      @data = CSV.open(filename[0], :headers => true, :header_converters => :symbol)
+
+      # @data.each do |line|
+      #   puts [line[:first_name]]
+      # end
     end
 
     def self.valid_parameters?(parameters)
